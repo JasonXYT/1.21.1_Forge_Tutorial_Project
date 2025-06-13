8e34f5a6c2ca70f88a2cd69720f359e7f593c490
Jason Xiao <xiao.jason2007@gmail.com>
2025-06-12 23:08:59 -0600

    Testing GitHub by adding a comment

--- src/main/java/net/jason/tutorialmod/TutorialMod.java
+++ src/main/java/net/jason/tutorialmod/TutorialMod.java
@@ -14,6 +14,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
+// Testing GitHub by adding a comment
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TutorialMod.MOD_ID)
 public class TutorialMod {
